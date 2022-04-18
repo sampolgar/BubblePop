e663c5491962fc4688174ab7455b5187a3429909
Sam Polgar <sampolgart@gmail.com>
2022-04-18 20:36:17 +1000

    multiple balloons, restructure code

--- BubblePop/Balloon.swift
+++ BubblePop/Balloon.swift
@@ -6,29 +6,42 @@
 //
 import SpriteKit
 
+let degreesToRadians = CGFloat.pi / 180
+let radiansToDegrees = 180 / CGFloat.pi
+
+let maxGameDuration = 30;
+let maxBalloonVelocity: CGFloat = 200
+let maxBalloonAcceleration: CGFloat = 400
+
+func random() -> CGFloat {
+  return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+}
+
+func random(min: CGFloat, max: CGFloat) -> CGFloat {
+  return random() * (max - min) + min
+}
+
+func /(point: CGPoint, scalar: CGFloat) -> CGPoint {
+  return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+
+
 class Balloon: SKSpriteNode {
     var balloonColor: String?
     var gameScore: Int?
-//    var balloonDirection: String
+    
     
     init () {
         let balloonSize = CGSize(width: 50, height: 50)
         
         super.init(texture: nil, color: UIColor.clear, size: balloonSize)
-        
         setup()
+        setupPhysics()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder: ) has not been implemented")
-    }
-    
-    private func setBalloonDirection(){
-        
-    }
-    
-    public func changeBalloonDirection(){
-        
     }
     
     private func setup() {
@@ -59,5 +72,35 @@ class Balloon: SKSpriteNode {
             self.balloonColor = "nil"
             self.gameScore = 0
         }
+    }
+    
+    private func setupPhysics(){
+        let defaultImage = SKTexture(imageNamed: "pinkBalloon.png")
+        let balloonX = random(min: self.size.width/2, max: (self.scene?.size.width ?? 100 - self.size.width/2))
+        let velocityX = random(min: -75, max: 75)
+        let velocityY = random(min: 125, max: 250)
+        print("velocityX is \(velocityX)   velocityY is \(velocityY)  balloonX is \(balloonX) ")
+        print(" size.width/2 \(self.size.width/2)  other \((self.scene?.size.width ?? 100) - self.size.width/2) ")
+        
+        self.position = CGPoint(x: balloonX, y: self.size.height)                 // create the balloon on the x/y axis near the bottom of the x
+        
+        //setup physics
+        //Friction = 0, Restitution = 1, linearDamping = 0, AngularDamping = 0, isDynamic = true, velocity
+        self.physicsBody = SKPhysicsBody(texture: self.texture ?? defaultImage, size: self.size)               // create a physics body around the balloon to let it move and interact
+        self.physicsBody?.friction = 0
+        self.physicsBody?.restitution = 1
+        self.physicsBody?.angularDamping = 0
+        self.physicsBody?.linearDamping = 0
+        self.physicsBody?.angularDamping = 0
+        self.physicsBody?.isDynamic = true
+        
+        
+        self.physicsBody?.velocity = CGVector(dx: velocityX, dy: velocityY)              //set starting velocity
+        
+            
+//        //add collision https://developer.apple.com/documentation/scenekit/scnphysicsbody/1514768-categorybitmask/
+//        self.physicsBody?.categoryBitMask = PhysicsCategory.balloon
+//        self.physicsBody?.contactTestBitMask =  PhysicsCategory.balloon
+//        self.physicsBody?.collisionBitMask = PhysicsCategory.none
     }
 }
--- BubblePop/GameScene.swift
+++ BubblePop/GameScene.swift
@@ -7,189 +7,73 @@
 //To do
 // Make single balloon move and bounce from the walls
 // Create 15 balloons
-//
-//
-//
 
-let degreesToRadians = CGFloat.pi / 180
-let radiansToDegrees = 180 / CGFloat.pi
-
-let maxGameDuration = 30;
-let maxBalloonVelocity: CGFloat = 200
-let maxBalloonAcceleration: CGFloat = 400
-
-func /(point: CGPoint, scalar: CGFloat) -> CGPoint {
-  return CGPoint(x: point.x / scalar, y: point.y / scalar)
-}
-
-//SpriteKit easy language for games on iOS
 import SpriteKit
-//Core Motion reports motion- and environment-related data from the onboard hardware of iOS
-import CoreMotion
 
-enum CollisionTypes: UInt32 {
-  case none = 0
-  case all = 10
-  case balloon = 0b1
+struct PhysicsCategory {
+  static let none      : UInt32 = 0
+  static let all       : UInt32 = UInt32.max
+  static let balloon   : UInt32 = 0b1       // 1
 }
 
 class GameScene: SKScene {
-//  let coreMotion = CMMotionManager()
-  
-  var balloonCount = 0;
-  let gameBalloon: Balloon
-  var balloonAcceleration = CGVector(dx: 0, dy: 0)
-  var balloonVelocity = CGVector(dx: 0, dy: 0)
+  var balloonCount = 0
   var lastUpdateTime: CFTimeInterval = 0
+  var sceneContent = false
   
-  required init?(coder aDecoder: NSCoder) {
-    gameBalloon = Balloon()
-    super.init(coder: aDecoder)
-    setup()
-  }
-  
-  override init() {
-    gameBalloon = Balloon()
-    super.init()
-    setup()
-  }
-  
-  override init(size: CGSize) {
-    gameBalloon = Balloon()
-    super.init(size: size)
-    setup()
-  }
-  
-  func random() -> CGFloat {
-    return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
-  }
-
-  func random(min: CGFloat, max: CGFloat) -> CGFloat {
-    return random() * (max - min) + min
-  }
-    
-  func setup() {
-    //add balloons until 15 are on the screen simultaenously
-    createSceneContents()
-    addBalloons()
+  override func didMove(to view: SKView) {
+    if (!self.sceneContent) {
+          self.createSceneContents()
+          self.sceneContent = true
+        }
+    physicsWorld.contactDelegate = self
     physicsWorld.gravity = .zero
-//    physicsWorld.contactDelegate = self
-//    SKAction.run {
-//      <#code#>
-//    }
+  }
+  
+  override func sceneDidLoad() {
+    setup15Ballons()
+  }
+  
+  override func update(_ currentTime: TimeInterval) {
+  }
+  
+  //check the user touched a balloon
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let touch = touches.first else { return }
+    let touchLocation = touch.location(in: self)
+    let touchSize = CGSize(width: 25,height: 25)
+    let touchNode = SKSpriteNode(color: SKColor.white, size: touchSize)
     
-    }
+    touchNode.physicsBody = SKPhysicsBody(circleOfRadius: 12.5)
+    addChild(touchNode)
+    touchNode.position = touchLocation
+  }
   
   func createSceneContents(){
-    self.backgroundColor = .black
-    self.scaleMode = .aspectFit
-    self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+    let borderBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+    borderBody.friction = 0
+    self.physicsBody = borderBody
   }
   
-  func addBalloons(){
-    //https://manualzz.com/doc/o/m7v9k/ios-swift-game-development-cookbook-chapterandnbsp%3B7.andnbsp%3Bphysics
-    //https://stackoverflow.com/questions/28831312/ios-spritekit-make-random-moving-of-sprites-and-something-like-collision-in-spac
-    //configure starting coordinates
-    let balloonX = random(min: gameBalloon.size.width/2, max: size.width - gameBalloon.size.width/2)
-    let velocityX = random(min: -75, max: 75)
-    let velocityY = random(min: 50, max: 125)
-    
-
-    //add the child
-    addChild(gameBalloon)
-    let defaultImage = SKTexture(imageNamed: "pinkBalloon.png")
-    gameBalloon.position = CGPoint(x: balloonX, y: gameBalloon.size.height)                 // create the balloon on the x/y axis near the bottom of the x
-    gameBalloon.physicsBody = SKPhysicsBody(texture: gameBalloon.texture ?? defaultImage, size: gameBalloon.size )               // create a physics body around the balloon to let it move and interact
-    gameBalloon.physicsBody?.isDynamic = true                                               // let the balloon move
-    gameBalloon.physicsBody?.velocity = CGVector(dx: velocityX, dy: velocityY)              //set starting velocity
-    
-    //add collision https://developer.apple.com/documentation/scenekit/scnphysicsbody/1514768-categorybitmask/
-    gameBalloon.physicsBody?.categoryBitMask = CollisionTypes.balloon.rawValue
-    gameBalloon.physicsBody?.contactTestBitMask =  CollisionTypes.balloon.rawValue
-    gameBalloon.physicsBody?.collisionBitMask = CollisionTypes.none.rawValue
-    
-//    gameBalloon.run(SKAction.sequence())
-    
+  func createBalloon(){
+    let balloon = Balloon()
+    balloon.name = "balloon"
+    balloonCount += 1
+    addChild(balloon)
   }
   
-  //updates the screen every second
-  override func update(_ currentTime: TimeInterval) {
-    let deltaTime = max(1.0/30, currentTime - lastUpdateTime)
-    lastUpdateTime = currentTime
-    updateBalloon(deltaTime)
-  }
-  
-  //run this very often to check where the gameBalloons are
-  func updateBalloon(_ dt: CFTimeInterval){
-    
-//    balloonVelocity.dx = balloonVelocity.dx * CGFloat(dt)         //playerVelocity.dx = playerVelocity.dx + playerAcceleration.dx * CGFloat(dt) - adds acceleration from core motion
-//    balloonVelocity.dy = balloonVelocity.dy * CGFloat(dt)         //playerVelocity.dy = playerVelocity.dy + playerAcceleration.dy * CGFloat(dt) - adds acceleration from core motion
-    
-    balloonVelocity.dx = max(-maxBalloonVelocity, min(maxBalloonVelocity, balloonVelocity.dx))
-    balloonVelocity.dy = max(-maxBalloonVelocity, min(maxBalloonVelocity, balloonVelocity.dy))
-    print("Velocities : x \(balloonVelocity.dx)    and y \(balloonVelocity.dy)")
-    
-    var newX = gameBalloon.position.x + balloonVelocity.dx * CGFloat(dt)
-    var newY = gameBalloon.position.y + balloonVelocity.dy * CGFloat(dt)
-    
-    print("newX : x \(newX)    and newY \(newY)")
-    
-    var hitVertical = false
-    var hitHorizontal = false
-    
-    //check
-    if newX < 0 {
-      newX = 0
-      hitHorizontal = true
-    } else if newX > size.width {
-      newX = size.width
-      hitHorizontal = true
+  func setup15Ballons(){
+    for _ in 1...15 {
+        createBalloon()
     }
-    
-    
-    if newY < 0 {
-      newY = 0
-      hitVertical = true
-    } else if newY > size.height {
-      newY = size.height
-      hitVertical = true
-    }
-    
-    //if balloon hits vertical, change y velocity and acceleration
-    if hitVertical {
-      print("Hit vert")
-      balloonVelocity.dx = balloonVelocity.dx
-      balloonVelocity.dy = -balloonVelocity.dy
-      
-    }
-    
-    //if balloon hits horizontal, change x velocity and acceleration
-    if hitHorizontal {
-      print("Hit Horitzontal")
-      //Change Velocity
-      balloonVelocity.dx = -balloonVelocity.dx
-      balloonVelocity.dy = balloonVelocity.dy
-            
-    }
-    
-    gameBalloon.position = CGPoint(x: newX, y: newY)
-    
-  }
-  //change the direction of the balloon
-//  let angle = atan2(playerVelocity.dy, playerVelocity.dx)
-//  print("angle is \(angle)")
-//  playerSprite.zRotation = angle - 90 * degreesToRadians
-  
-  
-  
-}
-
-extension CGPoint {
-  func length() -> CGFloat {
-    return sqrt(x*x + y*y)
   }
 }
 
 extension GameScene: SKPhysicsContactDelegate {
-  
+
 }
+
+
+
+
+
